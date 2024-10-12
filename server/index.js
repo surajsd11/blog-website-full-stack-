f0b1c20d5baa003d4584a667f95fc944bb1d1090
suraj singh <117906705+surajsd11@users.noreply.github.com>
2024-10-12 15:23:49 +0530

    cors changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = 9000;
 
 connectToMonogo();
-app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST","GET"],
+  credentials: true
+}));
 
 app.use(express.json());
 
